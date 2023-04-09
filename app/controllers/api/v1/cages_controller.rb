3136e5311698e9dde6fb6da54a4509b273c17bbf
jennifer <jennifer.e.barstow@gmail.com>
2023-04-09 15:04:03 -0600

    add filtering for cage power_status

--- app/controllers/api/v1/cages_controller.rb
+++ app/controllers/api/v1/cages_controller.rb
@@ -1,12 +1,22 @@
 class Api::V1::CagesController < ApplicationController
 
   def index
-    @cages = Cage.all
-    render json: @cages.as_json(
-      only: [:id, :name, :power_status],
-      include: { dinosaurs: { only: [:name, :diet],
-      include: { species: { only: [:name, :diet]} } } }
-    ), status: :ok
+    if params[:power_status].present? && valid_power_status_query?(params[:power_status])
+      filter_cages_by_power_status
+    else
+      @cages = Cage.all
+    end
+
+    if params[:power_status].present? && !valid_power_status_query?(params[:power_status])
+      render json: { error: "Please enter a valid power status" }, status: :unprocessable_entity
+
+    else
+      render json: @cages.as_json(
+        only: [:id, :name, :power_status],
+        include: { dinosaurs: { only: [:name, :diet],
+        include: { species: { only: [:name, :diet]} } } }
+      ), status: :ok
+    end
   end
 
   def create
@@ -46,5 +56,13 @@ class Api::V1::CagesController < ApplicationController
     
   def cage_params
     params.require(:cage).permit(:name, :power_status)
-  end    
+  end
+
+  def valid_power_status_query?(query)
+    return true unless !["active", "down"].include?(query)
+  end
+
+  def filter_cages_by_power_status
+    @cages = Cage.where(power_status: params[:power_status] )
+  end
 end
